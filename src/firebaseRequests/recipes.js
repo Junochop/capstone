51d4053cd443c97993f6bf9e2a9b1bfe1519dce6
Juno Lee <lejuno80@gmail.com>
2018-07-21 11:38:10 -0500

    read recipe jason file

---
+++ src/firebaseRequests/recipes.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import constants from '../constants';
+
+const getRequestAll = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${constants.firebaseConfig.databaseURL}/recipes.json`)
+      .then(res => {
+        const items = [];
+        if (res.data !== null) {
+          Object.keys(res.data).forEach(fbKey => {
+            res.data[fbKey].id = fbKey;
+            items.push(res.data[fbKey]);
+          });
+        }
+        console.error(items, 'hi');
+        resolve(items);
+
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+// const getRequestGarage = () => {
+//   return new Promise((resolve, reject) => {
+//     axios
+//       .get(`${constants.firebaseConfig.databaseURL}/garage.json`)
+//       .then(res => {
+//         const items = [];
+//         if (res.data !== null) {
+//           Object.keys(res.data).forEach(fbKey => {
+//             res.data[fbKey].id = fbKey;
+//             items.push(res.data[fbKey]);
+//           });
+//         }
+//         resolve(items);
+
+//       })
+//       .catch(err => {
+//         reject(err);
+//       });
+//   });
+// };
+
+export default { getRequestAll };
